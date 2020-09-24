ca70c66555cae3d757a79719b5e87880108e135e
petro_telishevskyi <PetroTelishevskyi@users.noreply.github.com>
2020-09-24 10:50:12 +0300

    master

---
+++ my-practice-app/src/components/CsvReader.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import CSVReader from "react-csv-reader";
+
+export default class CSVReader extends Component {
+
+    handleFileLoaded(data){
+        let obj = {};
+        let array = [];
+        let keyArray = [];
+        
+        for (let i = 0; i < data.length; i++) {
+          array[i] = data.map((val) => val[i]);
+          keyArray[i] = data.map((val) => val[i]);
+        }; 
+
+        for (let i = 0; i < data.length; i++) {
+          array = array.filter((val) => val[i] !== undefined);
+          keyArray = array.filter((val) => val[i] !== undefined);
+        };  
+
+        for (let i = 0; i < keyArray.length; i++) {
+          keyArray[i] = keyArray[[i][0]].shift();
+        };
+       
+        for (let i = 0; i < array.length; i++) {
+          obj[keyArray[i]] =  array[i];   
+        };
+        this.props.csvUpdater.emit('update', obj);
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <CSVReader
+                 cssClass="react-csv-input"
+                 label="Select CSV with your data"
+                 onFileLoaded={this.handleFileLoaded.bind(this)}
+                />
+            </div>
+        );
+    }
+}
--- my-practice-app/src/components/Editor.js
+++ my-practice-app/src/components/Editor.js
@@ -9,8 +9,31 @@ import {
     Button,
     PanelMenuWrapper,
 } from 'react-chart-editor';
+import Modal from 'react-awesome-modal';
+import CsvReader from '../components/CsvReader';
+
 
 export default class Editor extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible : false
+        }
+    }
+
+    openModal() {
+        this.setState({
+            visible : true
+        });
+    }
+
+    closeModal() {
+        this.setState({
+            visible : false
+        });
+    }
+
     render(){
         return(
             <PanelMenuWrapper>
@@ -18,7 +41,23 @@ export default class Editor extends Component {
                 <StyleTracesPanel group='Style' name='Traces' />
                 <StyleLayoutPanel group='Style' name='Layout' />
                 <StyleLegendPanel group='Style' name='Legend' />
-                <StyleAxesPanel group='Style' name='Axes' />                
+                <StyleAxesPanel group='Style' name='Axes' />
+                <SingleSidebarItem>
+                    <Button label='Import' onClick={() => this.openModal()}/>
+                    <Modal 
+                        visible={this.state.visible}
+                        width="500"
+                        height="350"
+                        effect="fadeInDown"
+                        onClickAway={() => this.closeModal()}
+                    >
+                        <div>
+                            <h1>Upload File</h1>
+                            <CsvReader csvUpdater={this.props.csvUpdater}/>
+                            <button onClick={() => this.closeModal()}>Close</button>
+                        </div> 
+                    </Modal>
+                </SingleSidebarItem>                
             </PanelMenuWrapper>
         );
     }
